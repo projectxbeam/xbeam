c8945d721db55d618c2a9b7c65058c1b6a209388
NeoCode <amecus79@gmail.com>
2019-05-31 21:42:56 +0200

    Added button functions

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,18 @@ function selectSongBlock(newBlock) {
     document.getElementById("selected-song-block").id = "";
     newBlock.id = "selected-song-block";
 }
+
+var h, w, x, y;
+
+function maximize() {
+    if (require('electron').remote.getCurrentWindow().isMaximized()) {
+        require('electron').remote.getCurrentWindow().setSize(w, h);
+        require('electron').remote.getCurrentWindow().setPosition(x, y, true);
+    } else {
+        h = require('electron').remote.getCurrentWindow().getBounds().height;
+        w = require('electron').remote.getCurrentWindow().getBounds().width;
+        x = require('electron').remote.getCurrentWindow().getBounds().x;
+        y = require('electron').remote.getCurrentWindow().getBounds().y;
+        require('electron').remote.getCurrentWindow().maximize();
+    }
+}
